ec35f1bb5a743fade7962043c07555e31001d21d
cyifuzo <jeancyifuzodamas@gmail.com>
2022-03-06 18:35:59 -0500

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 
 const App = () => {
   return <div>
-    <Login />
+    <Cart />
     </div>;
 };
 
